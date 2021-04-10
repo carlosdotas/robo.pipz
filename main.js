cc34ba4710ea47c6410ee86ab3dcef3edb2a16bb
Carlos Dotas <42626583+carlosdotas@users.noreply.github.com>
2021-04-10 04:35:04 -0300

    Correcoes finais

--- main.js
+++ main.js
@@ -20,18 +20,35 @@ for (var i = 0; i < 10; i++) {
 }
 */
 
+
+
+//CPF 1: 010.281.334.56 x
+//CPF 2: 005.387.021.25 x
+//CPF 3: 048.934.704.50 x
+//CPF 4: 457.643.524.87 + 
+//CPF 5: 087.861.074.02 x 
+//CPF 6: 302.904.224.34 <
+
+
 //Teste 2
-let cfp = '010.281.334.56';
-let pontos = 16;
+let cfp = '457.643.524.87';
+let pontos = '718450';
+var ini = 1;
 setInterval(function(){ 
 
+	
 	$.get( "http://127.0.0.1/teste.php", function( data ) {
 
-		if(data=='1'){
+
+		if(data=='1' && ini==1){
 			console.log('foi');
+			ini = 0;
 			$.post( "https://bo.pipz.com.br/qualificationPoints/transfer", { cpf: cfp, points: pontos }, function( data ) {
 				console.log(data);
+				ini = 0;
+
 			}, "json");
+			
 		}
 
 
